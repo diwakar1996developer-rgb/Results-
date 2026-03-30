94917bb1ac015befc4bd786b8d18907303b072ee
diwakar1996developer-rgb <diwakar1996developer@gmail.com>
2026-03-31 05:04:43 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'blink': 'blink 1s linear infinite',
+      },
+      keyframes: {
+        blink: { '0%, 100%': { opacity: 1 }, '50%': { opacity: 0 } }
+      }
+    },
+  },
+  plugins: [],
+}
